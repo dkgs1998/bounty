d22a38f65f597370d43e6e753be0e61da8a2fb68
Gilad Kleinman <dkgs1998@gmail.com>
2015-06-24 02:31:52 +0300

    commiting before maor the suker

--- website/app/models/answer.rb
+++ website/app/models/answer.rb
@@ -1,2 +1,3 @@
 class Answer < ActiveRecord::Base
+	belongs_to :hunter, class_name: "User", foreign_key: "hunter_id"
 end
--- website/db/schema.rb
+++ website/db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150623225450) do
+ActiveRecord::Schema.define(version: 20150623231100) do
+
+  create_table "answers", force: :cascade do |t|
+    t.string   "title"
+    t.text     "text"
+    t.integer  "hunter_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "bounties", force: :cascade do |t|
     t.integer  "poster_id"
