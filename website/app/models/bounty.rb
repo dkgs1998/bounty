348021fde108fa9efe2df0ac4e7ad12ca53bd3e9
Gilad Kleinman <dkgs1998@gmail.com>
2015-06-24 03:13:36 +0300

    oh fucking yea done with many to many asocsiactions you bastered

--- website/app/models/bounty.rb
+++ website/app/models/bounty.rb
@@ -1,4 +1,5 @@
 class Bounty < ActiveRecord::Base
 	belongs_to :user, class_name: "User", foreign_key: "poster_id"
-	has_and_belongs_to_many :hunters, 
+	has_and_belongs_to_many :hunters
+	has_and_belongs_to_many :tags
 end
